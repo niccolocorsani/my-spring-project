0e65518b8cf6a4c02ab4b2bb5d5e2318f881817b
niccolocorsani <79635059+niccolocorsani@users.noreply.github.com>
2021-11-15 16:05:35 +0100

    no message

--- src/main/java/com/example/spring/demo/MySpringProjectApplication.java
+++ src/main/java/com/example/spring/demo/MySpringProjectApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MySpringProjectApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MySpringProjectApplication.class, args);
 	}
--- src/test/java/com/example/spring/demo/controllers/client/ClientControllerTest.java
+++ src/test/java/com/example/spring/demo/controllers/client/ClientControllerTest.java
@@ -10,10 +10,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.example.spring.demo.controllers.client.ClientController;
 import com.example.spring.demo.services.client.ClientService;
-
-
 
 
 
