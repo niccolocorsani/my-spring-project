d3eb081ffded4d4d2bade3061ae23667e7cf988f
niccolocorsani <79635059+niccolocorsani@users.noreply.github.com>
2021-11-16 17:59:34 +0100

    no message

--- src/main/java/com/example/spring/demo/model/appoitmnent/Appointment.java
+++ src/main/java/com/example/spring/demo/model/appoitmnent/Appointment.java
@@ -1,10 +1,39 @@
 package com.example.spring.demo.model.appoitmnent;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Appointment {
 
+
+	@Id
+	private Long id;
 	private Long clientId;
 	private Long consultantId;
+
+	public Long getClientId() {
+		return clientId;
+	}
+
+	public void setClientId(Long clientId) {
+		this.clientId = clientId;
+	}
+
+	public Long getConsultantId() {
+		return consultantId;
+	}
+
+	public void setConsultantId(Long consultantId) {
+		this.consultantId = consultantId;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
 }
