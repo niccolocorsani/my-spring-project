56371b394b2a541189178fd50d42c728f33cc3ba
niccolocorsani <79635059+niccolocorsani@users.noreply.github.com>
2021-11-17 18:21:55 +0100

    Cambio da @Controller a @RestController

--- src/main/java/com/example/spring/demo/controllers/consultant/ConsultantController.java
+++ src/main/java/com/example/spring/demo/controllers/consultant/ConsultantController.java
@@ -1,8 +1,9 @@
 package com.example.spring.demo.controllers.consultant;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class ConsultantController {
 
 	
--- src/test/java/com/example/spring/demo/unit/controllers/client/ClientControllerTest.java
+++ src/test/java/com/example/spring/demo/unit/controllers/client/ClientControllerTest.java
@@ -1,15 +1,26 @@
 package com.example.spring.demo.unit.controllers.client;
 
 
-import org.junit.jupiter.api.Test;
+import static java.util.Arrays.asList;
+import static org.hamcrest.CoreMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.example.spring.demo.controllers.client.ClientController;
+import com.example.spring.demo.model.client.Client;
 import com.example.spring.demo.services.client.ClientService;
 
 
@@ -28,30 +39,35 @@ public class ClientControllerTest {
 
 	@Test
 	public void testAllClientsEmpty() throws Exception {
-	/*	this.mvc.perform(get("/api/clients")
-				.accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk())
-				.andExpect(content().json("[]"));llllllll
-				// the above checks that the content is an empty JSON list*/
-		assert(true);
-	}
+		System.err.println("oo");
 
-/*	@Test
-	public void testAllClientsNotEmpty() throws Exception {
-		when(clientService.getAllClients()).
-			thenReturn(asList(
-				new Client(1L, "Marco", "Rossi"),
-				new Client(2L, "Francesco", "bianchi")
-			));
-			
 		this.mvc.perform(get("/api/clients")
 				.accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
-				.andExpect(jsonPath("$[0].id", is(1)))
-				.andExpect(jsonPath("$[0].name", is("first")))
-				.andExpect(jsonPath("$[0].salary", is(1000)))
-				.andExpect(jsonPath("$[1].id", is(2)))
-				.andExpect(jsonPath("$[1].name", is("second")))
-				.andExpect(jsonPath("$[1].salary", is(5000)));
-	}*/
+				.andExpect(content().json("[]"));
+				// the above checks that the content is an empty JSON list
+		assert(true);
+	}
+
+	@Test
+	public void testAllClientsNotEmpty() throws Exception {
+		
+		List<Client> clients = new ArrayList<>();
+		clients.add(new Client(1L, "Marco", "Rossi"));
+		clients.add(new Client(2L, "Francesco", ""));
+		System.err.println("oo");
+
+		when(clientService.getAllClients()).
+			thenReturn(clients);
+	
+		
+		this.mvc.perform(get("/api/clients")
+				.accept(MediaType.APPLICATION_JSON_VALUE))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$[0].id", is(1L)))
+				.andExpect(jsonPath("$[0].firstName", is("Marco")))
+				.andExpect(jsonPath("$[0].lastName", is("Rossi")))
+				.andExpect(jsonPath("$[1].id", is(2L)))
+				.andExpect(jsonPath("$[1].firstName", is("Francesco")));
+	}
 }
