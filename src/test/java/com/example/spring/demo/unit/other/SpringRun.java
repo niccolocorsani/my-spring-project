891159872a53e67ef47c931eb403f6ebc39ce8e3
niccolocorsani <79635059+niccolocorsani@users.noreply.github.com>
2021-12-20 15:14:31 +0100

    no message

--- src/test/java/com/example/spring/demo/unit/other/SpringRun.java
+++ src/test/java/com/example/spring/demo/unit/other/SpringRun.java
@@ -2,6 +2,8 @@ package com.example.spring.demo.unit.other;
 
 
 import com.example.spring.demo.MySpringProjectApplication;
+import com.example.spring.demo.controllers.client.ClientController;
+import com.example.spring.demo.controllers.consultant.ConsultantController;
 import org.apache.catalina.core.ApplicationContext;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -9,14 +11,28 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @RunWith(SpringRunner.class)
 public class SpringRun {
 
 
     @Test
-    public void start(){
+    public void start() {
 
-      ConfigurableApplicationContext configurableApplicationContext =  SpringApplication.run(MySpringProjectApplication.class, null);
+        ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(MySpringProjectApplication.class);
+        assertNotNull(configurableApplicationContext);
     }
 
+
+
+    @Test
+    public void controllersInstances() {
+
+        ClientController clientController = new ClientController();
+        assertNotNull(clientController);
+        ConsultantController consultantController = new ConsultantController();
+        assertNotNull(consultantController);
+
+    }
 }
