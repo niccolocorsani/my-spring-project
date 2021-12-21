734ae56d05d479c3673af486eb517bd70897b0c9
niccolocorsani <79635059+niccolocorsani@users.noreply.github.com>
2021-12-21 19:35:32 +0100

    no message

--- src/test/java/com/example/spring/demo/unit/other/otherTest.java
+++ src/test/java/com/example/spring/demo/unit/other/otherTest.java
@@ -4,9 +4,6 @@ package com.example.spring.demo.unit.other;
 import com.example.spring.demo.MySpringProjectApplication;
 import com.example.spring.demo.controllers.client.ClientController;
 import com.example.spring.demo.controllers.consultant.ConsultantController;
-import com.example.spring.demo.model.consultant.Consultant;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.catalina.core.ApplicationContext;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 
