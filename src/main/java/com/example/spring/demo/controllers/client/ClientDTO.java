4d45e3e52887b5b4b5b1591377967bd3769d3909
niccolocorsani <79635059+niccolocorsani@users.noreply.github.com>
2021-12-21 14:55:13 +0100

    no message

--- src/main/java/com/example/spring/demo/controllers/client/ClientDTO.java
+++ src/main/java/com/example/spring/demo/controllers/client/ClientDTO.java
@@ -1,22 +1,9 @@
 package com.example.spring.demo.controllers.client;
 
-import com.example.spring.demo.model.appoitmnent.Appointment;
 import com.example.spring.demo.parent_model.ParentModel;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class ClientDTO extends ParentModel {
 
-    private Long id;
-
-    private String firstName;
-
-    private String lastName;
-
-    private String userName;
-
-    private List<Appointment> appointments = new ArrayList<>();
 
 
 }
--- src/main/java/com/example/spring/demo/controllers/consultant/ConsultantDTO.java
+++ src/main/java/com/example/spring/demo/controllers/consultant/ConsultantDTO.java
@@ -1,23 +1,10 @@
 package com.example.spring.demo.controllers.consultant;
 
-import com.example.spring.demo.model.appoitmnent.Appointment;
 import com.example.spring.demo.parent_model.ParentModel;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class ConsultantDTO extends ParentModel {
 
-
-    private Long id;
-
-    private String firstName;
-
-    private String lastName;
-
-    private String userName;
-
-    private List<Appointment> appointments = new ArrayList<>();
 
 
 }
